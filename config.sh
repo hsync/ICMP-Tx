7be2105b5667a0be3533a3d8789ffae601aedd05
hsync <felixschulze@hotmail.de>
2014-12-10 20:54:05 +0100

    update config.sh

--- config.sh
+++ config.sh
@@ -3,6 +3,6 @@
 #very quick, dirty and ugly ;)
 #find the default gateway and set it as dst for the first test
 echo -e -n 'IP:' > ./config
-route | grep default | cut -c17-32 | tr -s ' ' '\n' >> ./config
+route -v | grep default | cut -c17-32 | tr -s ' ' '\n' >> ./config
 echo -e 'ProxyPort:8080' >> ./config
 
