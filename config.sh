6d200135a1390471e609270dbb4f636812430c3a
hsync <felixschulze@hotmail.de>
2014-12-10 18:22:33 +0100

    add default gateway as standard test destination

--- config.sh
+++ config.sh
@@ -1,1 +1,7 @@
-echo -e 'IP:192.168.1.1' > ./config
+#echo -e 'IP:192.168.1.1' > ./config
+
+#very quick, dirty and ugly ;)
+#find the default gateway and set it as dst for the first test
+echo -e -n 'IP:' > ./config
+route | grep default | cut -c17-32 | tr -s ' ' '\n' >> ./config
+
