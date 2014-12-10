3b131019330f92cb7be612ace9fab19ea6a375cd
Alex Wellnitz <wellnitz.alex@gmail.com>
2014-12-10 09:28:02 +0100

    59,12 delete datan'[]' (SyntaxError).

--- start.py
+++ start.py
@@ -5,7 +5,7 @@ import struct
 
 import select
 import random
-import asyncore 
+import asyncore
 import time
 
 
@@ -20,7 +20,7 @@ def icmp_checksum(source_string):
 	while count < count_to:
 		this_val = ord(source_string[count + 1])*256+ord(source_string[count])
 		sum = sum + this_val
-		sum = sum & 0xffffffff 
+		sum = sum & 0xffffffff
 		count = count + 2
 	if count_to < len(source_string):
 		sum = sum + ord(source_string[len(source_string) - 1])
@@ -31,7 +31,6 @@ def icmp_checksum(source_string):
 	answer = answer & 0xffff
 	answer = answer >> 8 | (answer << 8 & 0xff00)
 	return answer
-  
 
 def sendPacket(dst_Ip, msg_fp):
 	id = 5032
@@ -40,16 +39,16 @@ def sendPacket(dst_Ip, msg_fp):
 	header = struct.pack('bbHHh', ICMP_ECHO_REQUEST, ICMP_ECHO_CODE, 0, id, 1)
 
 	# Calculate the checksum on the data and the dummy header.
-	my_checksum = icmp_checksum(header + msg_fp) 
+	my_checksum = icmp_checksum(header + msg_fp)
 
 	header = struct.pack('bbHHh', ICMP_ECHO_REQUEST, ICMP_ECHO_CODE, socket.htons(my_checksum), id, 1)
- 
+
 	connection.sendto(header+msg_fp, (dst_Ip, 0))
 
 
 
 msg = "Hello World"
-dst = '192.168.1.1'
+dst = '192.168.178.25'
 
 connection = socket.socket(proto = socket.IPPROTO_ICMP, type = socket.SOCK_RAW)
 sendPacket(dst, msg)
@@ -57,7 +56,7 @@ print "[ \033[32mDEBUG\033[0m ] DST_IP : " + dst
 print "[ \033[32mDEBUG\033[0m ] MSG    : " + str(len(msg)) + " Byte sent"
 datan = connection.recvfrom(100)
 
-print datan[]
+print datan
 
 
 connection.close()
